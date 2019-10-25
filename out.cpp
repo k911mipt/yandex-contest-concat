bda293da6496f4aba78c30aa65daadc4adf60380
rogoznyi <dmitrii.rogoznyi@loginom.ru>
2019-10-25 18:10:27 +0300

    add simple solution

---
+++ out.cpp
@@ -0,0 +1,17 @@
+
+// BEGIN INCLUDING test.h
+#ifndef TEST_H_
+#define TEST_H_
+
+#include <iostream>
+
+int main(int argc, char** argv);
+
+#endif // TEST_H_
+// END INCLUDING test.h
+
+
+int main(int argc, char** argv) {
+    std::cout << "It works!" << std::endl;
+    return 0;
+}
---
+++ test.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef TEST_H_
+#define TEST_H_
+
+#include <iostream>
+
+int main(int argc, char** argv);
+
+#endif // TEST_H_
